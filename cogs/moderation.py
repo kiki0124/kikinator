b410f64b74c9258159bb25d41618b289653f03dc
kiki124 <128392944+kiki0124@users.noreply.github.com>
2024-02-15 18:34:46 +0200

    Update moderation.py
    Added if statements to check:
    * If the user you are trying to kick/ban is the guild's owner,
    * If the bot's role is lower than the highest role of the user you are trying to kick/ban,
    * If your highest role is the same or lower than the highest role of the user you are trying to ban.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
 from discord import Member, TextChannel, app_commands
-from discord.ext.commands import has_permissions, has_guild_permissions, MissingPermissions, bot_has_permissions, BotMissingPermissions
+from discord.ext.commands import has_permissions, MissingPermissions, bot_has_permissions, BotMissingPermissions
 import os
 import asyncio
-
+import datetime
 
 class moderation(commands.Cog):
     def __init__(self, client):
@@ -14,53 +14,93 @@ class moderation(commands.Cog):
     @has_permissions(kick_members=True)
     @bot_has_permissions(kick_members=True)
     @app_commands.describe(user="What user do you want to kick?", reason="What is the reason for kicking this user?")
-    async def kick(self, ctx, user: Member, reason: str):
-        await user.kick(reason=reason)
-        await ctx.reply(f"{user.name} ({user.mention}) successfully kicked. \n Reason: {reason} \n Author: {ctx.author.name} ({ctx.author.mention}).")
-
-    @kick.error
-    async def kick_error(self, ctx, error):
-        if isinstance(error, MissingPermissions):
-            await ctx.reply("Insufficient permissions. \n This command requires `KICK_MEMBERS` permission.")
-        elif isinstance(error, BotMissingPermissions):
-            await ctx.reply("The bot needs to have `KICK_MEMBERS` permission and have a role above the highest role of this user for you to be able to execute this command.")
-        elif isinstance(error, discord.errors.NotFound):
-            await ctx.reply(f"User could not be found. Please try again later.")
-        
+    async def kick(self, ctx: commands.Context, user: Member, reason: str):
+        await ctx.defer()
+        if user.id == ctx.author.id:
+            embed = discord.Embed(
+                title="There was a problem...",
+                description="You cannot kick yourself.",
+                color=discord.Color.orange()
+            )
+            await ctx.reply(embed=embed)
+        elif user.id == ctx.guild.owner.id:
+            embed = discord.Embed(
+                title="Insufficient permissions.",
+                color=discord.Color.orange(),
+                description=f"You cannot kick {user.mention} because they are the owner of this server."
+            )
+            await ctx.reply(embed=embed)
+        elif user.top_role >= ctx.guild.me.top_role:
+            embed = discord.Embed(
+                title="Insufficient bot permissions.",
+                color=discord.Color.orange(),
+                description="The bot's highest role needs to be above the highest role of the user you are trying to kick."
+            )
+            await ctx.reply(embed=embed, mention_author=False)
+        elif user.top_role >= ctx.author.top_role:
+            embed = discord.Embed(
+                title="Insufficient permissions.",
+                description=f"{user.mention}'s highest role is your highest role or is higher than your highest role.",
+                color=discord.Color.orange()
+            )
+            await ctx.reply(embed=embed)
+        else:
+            await user.kick(reason=reason)
+            embed = discord.Embed(
+                title=f"{user.name} successfully kicked.",
+                color=0x00A8FB
+            )
+            embed.add_field(name="Reason:", value=reason)
+            embed.add_field(name="User:", value=f"{user.mention} | {user.name} | {user.id}")
+            embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar.url)
+            await ctx.reply(embed=embed)
+    
     @commands.hybrid_command(name="ban", description="Ban a user from your server.")
     @has_permissions(ban_members=True)
     @bot_has_permissions(ban_members=True)
     @app_commands.describe(user="What user do you want to ban?", reason="What is the reason for banning this user?")
-    async def ban(self, ctx, user: Member, reason: str = None):
-        reason = None if not reason else reason
-        await user.ban(reason=reason)
-        embed = discord.Embed(
-            title=f"User successfully banned.",
-            color=0x00A8FB
-        )
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
-        embed.add_field(name="Reason:", value=reason)
-    
-    @ban.error
-    async def ban_error(self, ctx, error):
-        if isinstance(error, MissingPermissions):
+    async def ban(self, ctx: commands.Context, user: Member, reason: str):
+        await ctx.defer()
+        if user.id == ctx.author.id:
             embed = discord.Embed(
-                title="Insufficient permissions.",
-                description="This command requires `BAN_MEMBERS` permission.",
+                title="There was a problem...",
+                description="You cannot ban yourself.",
                 color=discord.Color.orange()
             )
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.icon.url)
             await ctx.reply(embed=embed)
-        elif isinstance(error, BotMissingPermissions):
+        elif user.id == ctx.guild.owner.id:
             embed = discord.Embed(
                 title="Insufficient permissions.",
-                description=f"The bot requires `BAN_MEMBERS` permission, and it needs to have a role above the highest role of the user you are trying to ban.",
+                color=discord.Color.orange(),
+                description=f"You cannot ban {user.mention} because they are the owner of this server."
+            )
+            await ctx.reply(embed=embed)
+        elif user.top_role >= ctx.guild.me.top_role:
+            embed = discord.Embed(
+                title="Insufficient bot permissions.",
+                color=discord.Color.orange(),
+                description="Kikinator's highest role needs to be above the highest role of the user you are trying to ban."
+            )
+            await ctx.reply(embed=embed, mention_author=False)
+        elif user.top_role >= ctx.author.top_role:
+            embed = discord.Embed(
+                title="Insufficient permissions.",
+                description=f"{user.mention}'s highest role is your highest role or is higher than your highest role.",
                 color=discord.Color.orange()
             )
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
             await ctx.reply(embed=embed)
-    
-    @commands.hybrid_command(name="purge", description="Purge up to 99 messages at a time using the bot.")
+        else:
+            await user.ban(reason=reason)
+            embed = discord.Embed(
+                title=f"{user.name} successfully banned.",
+                color=0x00A8FB
+            )
+            embed.add_field(name="Reason:", value=reason)
+            embed.add_field(name="User:", value=f"{user.mention} | {user.name} | {user.id}")
+            embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar.url)
+            await ctx.reply(embed=embed)
+          
+    @commands.hybrid_command(name="purge", description="Purge up to 99 messages at a time using the bot.", aliases=["deletemessages", "delmsgs", "delmsg", "deletemsg", "deletemsgs", "clean", "clear"])
     @has_permissions(manage_messages=True)
     @bot_has_permissions(manage_messages=True)
     @app_commands.describe(amount="How many messages do you want to purge? maximum is 99.", channel="From what channel do you want to purge messages?")
@@ -76,26 +116,9 @@ class moderation(commands.Cog):
                 description=f'Successfully purged {amount} messages from {channel.mention}.',
                 color=0x00A8FB
             )
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
-            await ctx.channel.send(embed=embed)
-
-    @purge.error
-    async def purge_error(self, ctx, error):
-        if isinstance(error, MissingPermissions):
-            embed = discord.Embed(
-                title="Insufficient permissions.",
-                description="This command requires `MANAGE_MESSAGES` permission.",
-                color=discord.Color.orange()
-            )
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
-            await ctx.reply(embed=embed)
-        elif isinstance(error, BotMissingPermissions):
-            embed = discord.Embed(
-                title="Insufficient bot permissions.",
-                description="The bot needs to have `MANAGE_MESSAGES` permission for you to be able to use this command.",
-                color=discord.Color.orange()
-                )
-            await ctx.reply(embed=embed)
+            embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar.url)
+            await asyncio.sleep(3)
+            await channel.send(embed=embed)
 
 
 async def setup(client):
